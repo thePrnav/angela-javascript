8fbd450155a3942fe5ce1086ac6c09131b7f51f7
thePrnav <114599439+thePrnav@users.noreply.github.com>
2024-03-24 08:17:25 +0000

    random number genration

---
+++ randomNumberGenration.js
@@ -0,0 +1,19 @@
+// var n = Math.random();
+// n = n*6
+// n = Math.floor(n) + 1;
+// console.log(n);
+
+prompt("What is your name ?")
+prompt("What is your crush name ?")
+
+var loveScore = Math.random()*100;
+loveScore = Math.floor(loveScore) + 1;
+
+// using IF - Else statements
+
+
+if(loveScore > 70){
+    alert("Your love score is "+ loveScore+ "%"+ " You love each other like kanye like kanye.")
+}else{
+    alert("Your love score is "+ loveScore+ "%")
+}
