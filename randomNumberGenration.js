a493b4668b3c31c8adfa0f480bf9d8db1cdc5733
thePrnav <114599439+thePrnav@users.noreply.github.com>
2024-03-26 14:21:38 +0000

    Example of array

--- randomNumberGenration.js
+++ randomNumberGenration.js
@@ -3,17 +3,83 @@
 // n = Math.floor(n) + 1;
 // console.log(n);
 
-prompt("What is your name ?")
-prompt("What is your crush name ?")
+// prompt("What is your name ?")
+// prompt("What is your crush name ?")
 
-var loveScore = Math.random()*100;
-loveScore = Math.floor(loveScore) + 1;
+// var loveScore = Math.random()*100;
+// loveScore = Math.floor(loveScore) + 1;
 
-// using IF - Else statements
+// // using IF - Else statements
 
 
-if(loveScore > 70){
-    alert("Your love score is "+ loveScore+ "%"+ " You love each other like kanye like kanye.")
-}else{
-    alert("Your love score is "+ loveScore+ "%")
-}
+// if(loveScore > 70){
+//     alert("Your love score is "+ loveScore+ "%"+ " You love each other like kanye like kanye.")
+// }else{
+//     alert("Your love score is "+ loveScore+ "%")
+// }
+
+
+//function bmiCalculator(weight, height){
+    //     var bmi = weight/ (height*height);
+    //     bmi = Math.round(bmi);
+        
+    
+    // if(bmi <= 18.5){
+    //         console.log("Your BMI is " + bmi +", so you are underweight.")
+    //     }
+    //     else if(bmi <= 24.9 && bmi >= 18.5){
+    //         console.log("Your BMI is " + bmi + ", so you have a normal weight.")
+    //     }
+    //     else {
+    //         console.log("Your BMI is " + bmi + ", so you are overweight.")
+    //     }
+       
+    
+    // }
+    //  bmiCalculator(100,2)
+    
+
+
+    // function isLeap(year) {
+    
+    //     /**************Don't change the code above****************/    
+            
+    //         //Write your code here.    
+        
+    //     if(year % 4 === 0){
+    //         if(year % 100 == 0){
+    //             if(year % 400 == 0){
+    //                 return "Leap year."
+    //             }else {
+    //                 return "Not leap year."
+    //             }
+    //         }else {
+    //             return " Leap year."
+    //         }
+    //     }else {
+    //         return "Not leap year."
+    //     }
+            
+        
+    //     /**************Don't change the code below****************/    
+        
+    //     }
+        
+/********************* Example of array (How to use .includes )********************** */
+    // "Angela"
+    // "jack"
+    // "pam"
+    // "james"
+    // "lara"
+    // "jason"
+    
+    // var guestList = ["Angela","jack","pam","james","lara","jason"];
+    
+    // var guestName = prompt("What is your name ?");
+    // if(guestList.includes(guestName)){
+    //     alert("Welcome!");
+    // }else{
+    //     alert("Sorry may be next time")
+    // }
+    
+
